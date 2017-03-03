22ddc6e7a40a5e870b891902a7ce2ef389132ebb
Mingwei <Mingwei.Samuel@gmail.com>
2017-03-03 14:09:54 -0800

    re-add leaderboard api

---
+++ ChampionMains.Pyrobot/Models/LeaderboardViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ChampionMains.Pyrobot.Models
+{
+    public class LeaderboardViewModel
+    {
+        public string SortedBy => "mastery";
+        public int ChampionId { get; set; }
+        public IList<LeaderboardEntryViewModel> Entries { get; set; }
+    }
+
+    public class LeaderboardEntryViewModel
+    {
+        public string Name { get; set; }
+        public int TotalPoints { get; set; }
+    }
+}
