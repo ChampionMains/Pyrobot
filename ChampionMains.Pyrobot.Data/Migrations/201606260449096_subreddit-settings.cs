a540c921dfd05cde7f37a49cbd674aa6b301f570
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-02-05 17:36:05 -0800

    fix initial migration when columns missing

--- ChampionMains.Pyrobot.Data/Migrations/201606260449096_subreddit-settings.cs
+++ ChampionMains.Pyrobot.Data/Migrations/201606260449096_subreddit-settings.cs
@@ -10,9 +10,14 @@ namespace ChampionMains.Pyrobot.Data.Migrations
             AddColumn("dbo.SubReddit", "RankEnabled", c => c.Boolean());
             AddColumn("dbo.SubReddit", "ChampionMasteryEnabled", c => c.Boolean());
 
-            Sql("UPDATE dbo.SubReddit SET AdminOnly = 0 WHERE Identifier IS NULL");
-            Sql("UPDATE dbo.SubReddit SET RankEnabled = 1 WHERE Identifier IS NULL");
-            Sql("UPDATE dbo.SubReddit SET ChampionMasteryEnabled = 1 WHERE Identifier IS NULL");
+            Sql(@"
+IF EXISTS(SELECT 1 FROM SYS.COLUMNS
+	WHERE Name='Identifier'
+	AND OBJECT_ID = OBJECT_ID('dbo.SubReddit'))
+BEGIN
+	EXEC('UPDATE dbo.SubReddit SET AdminOnly = 0, RankEnabled = 1, ChampionMasteryEnabled = 1
+    WHERE Identifier IS NULL')
+END");
 
             AlterColumn("dbo.SubReddit", "AdminOnly", c => c.Boolean(nullable: false));
             AlterColumn("dbo.SubReddit", "RankEnabled", c => c.Boolean(nullable: false));
