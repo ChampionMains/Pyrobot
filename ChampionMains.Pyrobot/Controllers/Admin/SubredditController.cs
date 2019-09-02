b610392efafc5993d30bb64e83fafbeec0a0341d
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-09-02 15:14:48 -0700

    wip for new admin page

---
+++ ChampionMains.Pyrobot/Controllers/Admin/SubredditController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ChampionMains.Pyrobot.Controllers.Admin
+{
+    public class SubredditController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
