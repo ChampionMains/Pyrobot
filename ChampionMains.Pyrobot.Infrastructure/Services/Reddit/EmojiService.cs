07a837c861a01595205a5d7d1f6ef5d765ff89eb
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-09-07 21:18:31 -0700

    adding emoji fetch

---
+++ ChampionMains.Pyrobot.Infrastructure/Services/Reddit/EmojiService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Reddit.Things;
+
+namespace ChampionMains.Pyrobot.Services.Reddit
+{
+    public class EmojiService
+    {
+        private readonly RedditApiProvider _redditApiProvider;
+
+        public EmojiService(RedditApiProvider redditApiProvider)
+        {
+            _redditApiProvider = redditApiProvider;
+        }
+
+        public async Task<List<Snoomoji>> GetEmoji(string subredditName)
+        {
+            var reddit = await _redditApiProvider.GetRedditApi();
+
+            var emojiContainer = JsonConvert.DeserializeObject<SnoomojiContainer>(
+                await reddit.Models.Emoji.ExecuteRequestAsync("api/v1/" + subredditName + "/emojis/all"));
+
+            return emojiContainer.SubredditEmojis?.Values.ToList();
+        }
+    }
+}
